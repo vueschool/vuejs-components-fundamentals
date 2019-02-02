6fccd9d26287ea076a78d42640cf2272d432ee25
Alex Kyriakidis <hootlex@icloud.com>
2019-02-02 18:16:13 +0100

    components within components

--- plan-picker/app.js
+++ plan-picker/app.js
@@ -1,3 +1,12 @@
+Vue.component('plan-picker', {
+  template: '#plan-picker-template',
+  data() {
+    return {
+      plans: ['The Single', 'The Curious', 'The Addict']
+    }
+  }
+})
+
 Vue.component('plan', {
   template: '#plan-template',
   props: {
@@ -9,8 +18,5 @@ Vue.component('plan', {
 })
 
 new Vue({
-  el: '#app',
-  data: {
-    plans: ['The Single', 'The Curious', 'The Addict']
-  }
+  el: '#app'
 })
