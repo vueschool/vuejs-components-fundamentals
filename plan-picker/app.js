b4424d67e24a8f50338aa1d9ba779e86e83a859e
Alex Kyriakidis <hootlex@icloud.com>
2019-02-02 18:28:53 +0100

    global vs local components

--- plan-picker/app.js
+++ plan-picker/app.js
@@ -1,13 +1,4 @@
-Vue.component('plan-picker', {
-  template: '#plan-picker-template',
-  data() {
-    return {
-      plans: ['The Single', 'The Curious', 'The Addict']
-    }
-  }
-})
-
-Vue.component('plan', {
+let PlanComponent = {
   template: '#plan-template',
   props: {
     name: {
@@ -15,8 +6,24 @@ Vue.component('plan', {
       required: true
     }
   }
-})
+}
+
+let PlanPickerComponent = {
+  template: '#plan-picker-template',
+  components: {
+    plan: PlanComponent
+  },
+  data() {
+    return {
+      plans: ['The Single', 'The Curious', 'The Addict']
+    }
+  }
+}
+
 
 new Vue({
-  el: '#app'
+  el: '#app',
+  components: {
+    'plan-picker': PlanPickerComponent
+  }
 })
