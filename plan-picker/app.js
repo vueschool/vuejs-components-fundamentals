65cfac0f6850693873e41aca12b36c58f3eae1d1
Alex Kyriakidis <hootlex@icloud.com>
2019-03-13 14:41:35 +0100

    component names: best practices

--- plan-picker/app.js
+++ plan-picker/app.js
@@ -1,5 +1,5 @@
-let PlanComponent = {
-  template: '#plan-template',
+let PlanPickerItemComponent = {
+  template: '#plan-picker-item-template',
   props: {
     name: {
       type: String,
@@ -24,7 +24,7 @@ let PlanComponent = {
 let PlanPickerComponent = {
   template: '#plan-picker-template',
   components: {
-    plan: PlanComponent
+    'plan-picker-item': PlanPickerItemComponent
   },
   data() {
     return {
